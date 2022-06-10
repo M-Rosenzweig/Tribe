28d2c892fa09277d6f827cf0579c6d1228b1e718
Moshe <moisherosenzweig@gmail.com>
2022-06-09 22:37:04 -0400

    end of day

--- frontend/src/pages/AppComingSoon.js
+++ frontend/src/pages/AppComingSoon.js
@@ -20,7 +20,8 @@ function AppComingSoon() {
 
       <div>
         <h1>My Tribes</h1>
-        <p>Tribe Members are look out for each other and no one is left. Easily Slide into your different Tribes dependant on goal and focus of each group. </p>
+        {/* <p>Tribe Members look out for each other and no one is left. Easily Slide into your different Tribes dependant on goal and focus of each group. </p> */}
+        <p> A Tribe is much more than a group - it is a unit that holds each other accountable, shares resources, and makes sure no one is left behind. Join a Tribe or found one.</p>
       </div>
 
       <div className="AppleAppStore"></div>
