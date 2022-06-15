39d6461d3526f666f44bbfb902db32807466e23a
Moshe <moisherosenzweig@gmail.com>
2022-06-15 12:06:19 -0400

    slight lift for bottom text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,10 +15,10 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
 
-  const [isDesktop, setDesktop] = useState(window.innerWidth > 420);
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 770);
 
   const updateMedia = () => {
-    setDesktop(window.innerWidth > 420);
+    setDesktop(window.innerWidth > 770);
   };
 
   useEffect(() => {
